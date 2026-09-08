daf1920650b5eda13095335e09ef3bcf0ddbbcdd
artCBranco <artcbranco@gmail.com>
2026-09-08 16:58:52 -0400

    Added some modifications to the pyramid code
    Levels 16 x 16 instead of 48 x 48, changed background color and levels

--- hello-world/version-control-workflow/js/script.js
+++ hello-world/version-control-workflow/js/script.js
@@ -1,24 +1,67 @@
 /**
- * Title of Project
- * Author Name
+ * Git Workflow Example
+ * Pippin Barr
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Some sample code for playing with version control.
+ * Draws a pyramid in the centre of the canvas and a
+ * red circle at the user's mouse position.
  */
 
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Create a canvas, hides the cursor
 */
 function setup() {
+    // A 900x640 canvas
+    createCanvas(900, 600
+    );
 
+    // Don't show the cursor
+    noCursor();
 }
 
-
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * Draws a top-down view of a pyramid and also a red circle
+ * at the position of the user's cursor
 */
 function draw() {
+    // Make the background black (specified as RGB)
+    background(255, 255, 255);
 
+    // Draw a pyramid
+    // How many levels for the pyramid
+    const levels = 50;
+    // Loop through every level (backwards)
+    for (let level = levels; level > 0; level--) {
+        // Draw this layer
+        push();
+        // Set the grey shade of the level based on its number
+        // e.g. level 1 will get a shade of 10 (dark gray), 
+        // level 10 will be 255(white)
+        const shade = map(level, 1, levels, 10, 255);
+        // No line around the levels
+        noStroke();
+        // Set the fill colour to our shade (RGB)
+        fill(shade, shade, 0);
+        // Draw rectangles from the centre
+        rectMode(CENTER);
+        // Draw the rectangle in the centre of the canvas
+        // (320, 320) with a size based on the level
+        // e.g. level 1 will be a 48x48 rectangle and
+        // level 10 will be a 480x480 rectangle
+        rect(320, 320, level * 16, level * 16
+        );
+        pop();
+    }
+
+    // Draw a red circle at the position of the mouse
+    push();
+    // No line around the shape
+    noStroke();
+    // Make it red (RGB)
+    fill(255, 0, 0);
+    // Draw a 100x100 circle at the mouse position
+    ellipse(mouseX, mouseY, 100, 100);
+    pop();
 }
