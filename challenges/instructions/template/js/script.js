c7bd6fcdaf14db1a2c9f16cfd9b7b6c942f7a8eb
artCBranco <artcbranco@gmail.com>
2026-09-18 18:37:23 -0400

    feat: Adding Clipped Mask to create sun rays

--- challenges/instructions/template/js/script.js
+++ challenges/instructions/template/js/script.js
@@ -36,16 +36,17 @@ function drawSky() {
     ellipse(50, 400, 200)
     ellipse(350, 425, 200)
 
+    fill("#c0cdd8")
+    ellipse(100, 400, 125)
+    fill("#e1e9f0")
+    ellipse(100, 425, 125)
+
     // ▽ Drawing the right bg cloud 
     fill("#c0cdd8")
     ellipse(900, 500, 150)
     ellipse(800, 475, 125)
     ellipse(900, 450, 125)
     ellipse(1000, 450, 200)
-
-    // ▽ Drawing the sun
-    fill("#fedf8c")
-    ellipse(725, 700, 150)
 
 }
 
@@ -63,11 +64,45 @@ function drawMountain() {
     // ▽ Drawing the ground
     rect(540, 992, 1080, 200)
 
+}
+
+function drawSun() {
+    // ▽ Mask only works  inside push/pop
+    push()
+
+    // ▽ Drawing a Mask over the Rays
+    beginClip({ invert: true })
+
+    // ▽ Draw the cardinal directions
+    ellipse(725, 825, 90)
+    ellipse(725, 575, 90)
+    ellipse(600, 700, 90)
+    ellipse(850, 700, 90)
+
+    // ▽ Draw the Secondary directions
+    ellipse(825, 600, 110)
+    ellipse(625, 600, 110)
+    ellipse(825, 800, 110)
+    ellipse(625, 800, 110)
+
+    endClip()
+
+    // ▽ Drawing the sun rays
+    fill("#ffbb00")
+    ellipse(725, 700, 250)
+
+    // ▽ Drawing the sun
+    fill("#fedf8c")
+    ellipse(725, 700, 130)
+
+    pop()
 
 }
 
 function draw() {
+
     drawSky()
     drawMountain()
+    drawSun()
 
 }
