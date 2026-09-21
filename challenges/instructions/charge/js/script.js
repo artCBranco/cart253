ffc43978631344283c498350d5568b46d0ece199
artCBranco <artcbranco@gmail.com>
2026-09-21 17:26:52 -0400

    feat: Added loading bar

--- challenges/instructions/charge/js/script.js
+++ challenges/instructions/charge/js/script.js
@@ -12,29 +12,29 @@ function setup() {
 
     // ▽ Canvas fullscreen + Frame
     createCanvas(windowWidth, windowHeight);
-    
+
     // ▽ Loading speed
-    frameRate (20)
-    
+    frameRate(5)
+
+
     // ▽ Setting up auto-shape settings
     noStroke()
-    rectMode(CENTER)
-    
+
 }
 
 function draw() {
     background("#060606");
-    
+
     // ▽ horizontal distance is equal to 1/6th of the screen (to give a margin) + movement growing
-    let x = windowWidth / 6 + frameCount ^ 5
-    
-    // ▽ ever growing circle  
-    circle(x + 100, windowHeight / 2, 50)
-    
+    let width = windowWidth / 6 + frameCount ^ 5
+
+    // ▽ ever growing bar  
+    rect(windowWidth / 6, windowHeight / 2, width, 50, 100)
+
     // ▽ Loading text
     fill('#FFFFFF')
-    text("Loading...", windowWidth / 2, windowHeight / 3,)
-    textSize(64)
-    
-    
+    text("Loading...", windowWidth / 2, windowHeight 4 / 6,)
+    textSize(windowWidth / 64)
+
+
 }
