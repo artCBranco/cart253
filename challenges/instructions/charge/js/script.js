02ea381dd9a23ec38dbad593ca37a4479eecd450
artCBranco <artcbranco@gmail.com>
2026-09-22 10:45:29 -0400

    feat: If-Else statement now works

--- challenges/instructions/charge/js/script.js
+++ challenges/instructions/charge/js/script.js
@@ -23,24 +23,40 @@ function setup() {
 
 }
 
-
 function draw() {
 
-    let x = 150 + frameCount
+    let x = 150 + frameCount * 3
 
-    // ▽ Setting up auto-growing square
-    fill(255, 255, 255)
-    square(x, 740, 25, 100)
 
-    if (square(x) = 930) {
 
-        drawImage()
+    // ▽ If the loading bar reaches 100%
+    if (x > 930) {
+
+        // ▽ Draw a new frame
+        fill("#FFFFFF")
+        square(1080, 1080, 540)
+
+        // ▽ Hello world text
+        fill("#060606")
+        textSize(64)
+        text('hello world', 540, 540)
 
     }
 
-    // ▽ Loading text
-    textSize(64)
-    text('Loading', 540, 540)
+    else {
+
+
+
+        // ▽ Loading text
+        fill("#FFFFFF")
+        textSize(64)
+        text('Loading', 540, 540)
+
+        // ▽ Setting up auto-growing square
+        fill("#FFFFFF")
+        square(x, 740, 25, 100)
+    }
+
 
 
 }
