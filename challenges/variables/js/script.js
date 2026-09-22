9380f6948e3110e3a2c01b87a942152a3bdd484d
artCBranco <artcbranco@gmail.com>
2026-09-22 14:13:53 -0400

    feat: Pasted code given by Professor

--- challenges/variables/js/script.js
+++ challenges/variables/js/script.js
@@ -1,19 +1,41 @@
 /**
  * Variables - Mr. Red
+ * by Pippin Bar
+ * Make Mr. Furious Furious
+ * 
  * Felipe A. Castelo Branco and Felipe S. Paiva
  * 
- * Make Mr. Furious Furious
  */
 
 "use strict";
 
-// ▽ setting up the canvas
-function setup() {
+// ▽ Our friend Mr. Furious
+let mrFurious = {
+    // Position and size
+    x: 200,
+    y: 200,
+    size: 100,
+    // Colour
+    fill: {
+        r: 255,
+        g: 225,
+        b: 225
+    }
+};
 
+// ▽ Create the canvas
+function setup() {
+    createCanvas(400, 400);
 }
 
-
-// ▽ setting up the draw function
+// ▽ Draw (and update) Mr. Furious
 function draw() {
+    background(160, 180, 200);
 
+    // ▽ Draw Mr. Furious as a coloured circle
+    push();
+    noStroke();
+    fill(mrFurious.fill.r, mrFurious.fill.g, mrFurious.fill.b);
+    ellipse(mrFurious.x, mrFurious.y, mrFurious.size);
+    pop();
 }
