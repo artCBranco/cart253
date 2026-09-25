e32d89149e1a84f2c59ecfc97b5b8c1b49716d4f
artCBranco <artcbranco@gmail.com>
2026-09-24 21:09:42 -0400

    feat: Created a background and Mouse tracking

--- pr/challenges/variables/let-there-be/js/script.js
+++ pr/challenges/variables/let-there-be/js/script.js
@@ -7,15 +7,50 @@
 
 "use strict";
 
+let canvasW = 1920
+let canvasH = 1080
 
 function setup() {
+    // Create a full screen canvas
+    createCanvas(canvasW, canvasH)
+    // Background color
+    background(20, 20, 20)
+
+    // Rectangle alignment, remove cursor
+    rectMode(CENTER)
+}
+
+// Simple Background
+function drawDarkBG() {
+
+    // Darker wall
+    fill(15, 15, 15)
+    rect(canvasW / 6 * 5, canvasH / 2, canvasW / 6 * 2.5, canvasH)
+
+    // Lighter ground
+    fill(40, 40, 40)
+    rect(canvasW / 2, canvasH / 6 * 5.5, canvasW, canvasH / 6)
 
 }
 
+// Simple Background
+function drawLightBG() {
 
-/**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
+    // Darker wall
+    fill(5, 5, 5)
+    rect(canvasW / 6 * 5, canvasH / 2, canvasW / 6 * 2.5, canvasH)
+    // Lighter ground
+    fill(40, 40, 40)
+    rect(canvasW / 2, canvasH / 6 * 5.5, canvasW, canvasH / 6)
+
+}
+
+function drawLanternDark() {
+    fill(160, 160, 160)
+    circle(mouseX, mouseY, 100)
+}
+
 function draw() {
-
+    drawDarkBG()
+    drawLanternDark()
 }
