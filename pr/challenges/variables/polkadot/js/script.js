f2b4752a2e107b4409e5a9f18487f2b458aec509
artCBranco <artcbranco@gmail.com>
2026-09-26 18:00:58 -0400

    feat: Completed the project
    - Made a variable for the canvas properties
    - Fixed an issue where the textbox was being colored by the fill rather than the text

--- pr/challenges/variables/polkadot/js/script.js
+++ pr/challenges/variables/polkadot/js/script.js
@@ -1,34 +1,56 @@
-/**
- * Title of Project
- * Author Name
- * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+/*
+ * Polka Dot Abstract
+ * by Felipe Amorim Castelo Branco
  */
+
+/*
+Polka Dot Abstract
+by Felipe Amorim Castelo Branco
+*/
 
 "use strict";
 
-// Canvas size
-let canvasW = 1080
-let canvasH = 1080
+// Canvas properties.
+let canvas = {
+    x: 1080,
+    y: 1080,
+    fill: {
+        r: 245,
+        g: 245,
+        b: 255
+    }
+}
 
 function setup() {
-    // Setup Canvas
-    createCanvas(canvasW, canvasH)
-    background('#fffdf4')
+    // Setup Canvas.
+    createCanvas(canvas.x, canvas.y)
+    background(canvas.fill.r, canvas.fill.g, canvas.fill.b)
 
-    frameRate(60)
-    // Basic Settings
+    frameRate(24)
+    // Basic Settings.
     rectMode(CENTER)
     noStroke()
 }
 
+function drawTxt() {
+    //Text settings.
+    textSize(18)
+    textAlign(CENTER, CENTER)
+    textFont('Helvetica')
 
-function mousePressed() {
-    background('#fffdf4')
-    rect(canvasW / 2, canvasH / 2, canvasW / random(1, 5), canvasH / random(1, 5))
+    //Text 1 - black.
+    fill(0, 0, 0)
+    text("Draw anything!", canvas.x / 2, canvas.y / 2)
+    //Text 2 - white.
+    fill(canvas.fill.r, canvas.fill.g, canvas.fill.b)
+    text("press [SPACEBAR] to save", canvas.x / 2, canvas.y / 2 * 1.05)
 }
 
+//Draw a random sized, rando colored square in the background centered.
+function mousePressed() {
+    background(canvas.fill.r, canvas.fill.g, canvas.fill.b)
+    rect(canvas.x / 2, canvas.y / 2, canvas.x / random(1, 5), canvas.y / random(1, 5))
+}
 
 function keyPressed() {
     if (keyCode === 32) {
@@ -37,8 +59,10 @@ function keyPressed() {
 }
 
 function draw() {
-    fill(map(mouseY, 0, canvasH, 150, 255) + random(-50, 50), map(mouseY, 0, canvasH, 150, 255) + random(-50, 50), map(mouseY, 0, canvasH, 150, 255) + random(-50, 50))
-    circle(mouseX, mouseY, canvasH / 20 * random(0, 1))
+    drawTxt()
 
+    //Random soft colors.
+    fill(map(mouseY, 0, canvas.y, 150, 255) + random(-50, 50), map(mouseY, 0, canvas.y, 150, 255) + random(-50, 50), map(mouseY, 0, canvas.y, 150, 255) + random(-50, 50))
+    //Circle over the mouse.
+    circle(mouseX, mouseY, canvas.y / 20 * random(0, 1))
 }
-
