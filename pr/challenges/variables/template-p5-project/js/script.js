9a114362b4ccb285f58565133bf121cba01fa192
artCBranco <artcbranco@gmail.com>
2026-09-26 14:35:00 -0400

    feat:  added circles drawn on mouse position in random colors and shapes

--- pr/challenges/variables/template-p5-project/js/script.js
+++ pr/challenges/variables/template-p5-project/js/script.js
@@ -8,17 +8,25 @@
 
 "use strict";
 
-/**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
-function setup() {
+// Canvas size
+let canvasW = 1080
+let canvasH = 1080
 
+function setup() {
+    // Setup Canvas
+    createCanvas(canvasW, canvasH)
+    background('#fffdf4')
+
+    frameRate(60)
+    // Basic Settings
+    rectMode(CENTER)
+    noStroke()
 }
 
 
-/**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
 function draw() {
+
+    fill(map(mouseY, 0, canvasH, 100, 255) + random(-50, 50), map(mouseY, 0, canvasH, 100, 255) + random(-50, 50), map(mouseY, 0, canvasH, 100, 255) + random(-50, 50))
+    circle(0 + mouseX, mouseY, canvasH / 20 * random(0, 1))
 
 }
